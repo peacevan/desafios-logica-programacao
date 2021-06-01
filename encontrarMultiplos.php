deda83ade021467840bfde1f22be43b5bb2a176c
peacevan\amado <peacevan@gmail.com>
2021-05-31 23:13:47 -0300

    encontrar o multiplo de uma sequencia de numero  e sequencia de fibonacci

--- encontrarMultiplos.php
+++ encontrarMultiplos.php
@@ -1,15 +1,30 @@
 <?php
 
-class desafioNumeroMutiplo {
-    
-    static function findMultiploOfNumber($maxMum,$multiplo) {
+/*
+ * encontrar o multiplo de um determinado numero pesquisando  de 1 ate maxNum
+ */
+
+class desafios {
+
+    static function findMultiploOfNumber($maxMum, $multiplo):void {
         for ($i = 1; $i <= $maxMum; $i++) {
             $mode = $i % $multiplo;
-            echo($mode == 0) ? $i . ' é multilplo de 3 <br>' : $i .' não é<br>';
+            echo($mode == 0) ? $i . ' é multilplo de 3 <br>' : $i . ' não é<br>';
         }
     }
+
+    static function fibonacci() {
+       
+        $fib = array();
+        //ternário; 
+        for ($i = 0; $i < 15; $i++) {
+            $fib[$i] = ($i<2) ? 1 : ($fib[$i-1] + $fib[$i-2]);
+            print_r('<br>'.$fib[$i].'<br>');
+        }
+        return $fib;
+    }
+       
+
 }
 
-var_dump($_SERVER["REQUEST_URI"]);
-
-desafioNumeroMutiplo::findMultiploOfNumber(100,3);
+desafios::fibonacci();
--- index.php
+++ index.php
@@ -4,9 +4,16 @@ class desafioNumeroMutiplo {
     static function findMultiploOfNumber($maxMum,$multiplo) {
         for ($i = 1; $i <= $maxMum; $i++) {
             $mode = $i % $multiplo;
-            echo($mode == 0) ? $i . ' é multilplo de 3 <br>' : $i .'não é';
+            echo($mode == 0) ? $i . ' é multilplo de'. $multiplo.'<br>' : $i .'não é';
         }
     }
 }
 
-desafioNumeroMutiplo::findMultiploOfNumber(100,3);
+var_dump($_SERVER["REQUEST_URI"]);
+desafioNumeroMutiplo::findMultiploOfNumber(100,5);
+ function run(){
+     
+     
+     
+     
+ }
